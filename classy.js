9b52b9a372a3cfd7c73541b81d062fafb7be01d1
Evan Hahn <me@evanhahn.com>
2013-03-02 09:35:48 -1000

    remove private variable stuff for now

--- classy.js
+++ classy.js
@@ -1,38 +1,73 @@
-/* Classy.js
-   by Evan Hahn */
+(function(global) {
 
-var Classy = function() {};
+	// `Classy` is just the base class for everything, and its constructor is
+	// empty.
+	var Classy = function() {};
 
-Classy.extend = function(hash) {
+	// `extend()` is the exciting method that's inherited by everything.
+	Classy.extend = function(hash) {
 
-	// Generate the result
-	var result = (function closure(par, ctor, privates) {
+		// Generate the resulting object.
+		return (function closure(par, ctor) {
 
-		// Constructor
-		if (!ctor)
-			ctor = function() {};
-		var object = function() {
-			par.call(this, arguments);
-			ctor.call(this, arguments);
-		};
+			// If no constructor is defined, define an empty one.
+			if (!ctor)
+				ctor = function() {};
 
-		// Inherit all the properties
-		for (var property in par.prototype)
-			object.prototype[property] = par.prototype[property];
-		for (var property in hash)
-			object.prototype[property] = hash[property];
-		object.extend = Classy.extend;
+			// Create the constructor, which calls the parent constructor and the
+			// child one.
+			var Class = function() {
+				par.apply(this, arguments);
+				ctor.apply(this, arguments);
+			};
 
-		// Give the closure all of the private properties
-		for (var property in privates)
-			closure[property] = privates[property];
+			// Inherit all the properties from the parent.
+			for (var property in par.prototype)
+				Class.prototype[property] = par.prototype[property];
 
-		// Return the constructor
-		return object;
+			// Get all the properties from the new hash.
+			var existingFunctions = methods = {};
+			for (var property in hash) {
 
-	})(this, hash.init, hash.privates);
+				// If we already have a property in there that's a function, the child
+				// needs a `sup()` method in there.
+				var existingFunctions = methods = {};
+				existingFunctions[property] = par.prototype[property];
+				methods[property] = hash[property];
+				if (typeof existingFunctions[property] === 'function') {
+					Class.prototype[property] = function() {
+						this.sup = function() {
+							existingFunctions[property].apply(this, arguments);
+						};
+						return methods[property].apply(this, arguments);
+					};
+				}
 
-	// Return the result!
-	return result;
+				// We didn't have that existing parent function, so we're good.
+				else {
+					Class.prototype[property] = function() {
+						this.sup = void 0;
+						return methods[property].apply(this, arguments);
+					};
+				}
 
-};
+			}
+
+			// Make sure to get `extend()` into the class.
+			Class.extend = Classy.extend;
+
+			// Return the constructor (with attached prototype stuff).
+			return Class;
+
+		})(this, hash.init);
+
+	};
+
+	// Export everything.
+	if (module != null) {
+		module.exports = Classy;
+	} else {
+		// this.Classy = Classy;
+	}
+
+})(this);
