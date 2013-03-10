75a5a1d7e0c19a32b8249d1938f8da68de784a13
Evan Hahn <me@evanhahn.com>
2013-03-09 21:30:29 -1000

    second test for one-level-deep extending

--- test/test.js
+++ test/test.js
@@ -4,13 +4,30 @@ var Classy = require("../classy.js");
 
 describe("Classy", function() {
 
+	var undefined;
+
 	it("should allow classes to be created", function() {
 		var foo = new Classy;
 		var bar = new Classy;
 		foo.x = 12;
 		bar.x = 8;
-		expect(foo.x).equal(12);
-		expect(bar.x).equal(8);
+		expect(foo.x).eql(12);
+		expect(bar.x).eql(8);
+	});
+
+	it("allows subclasses to be created, one level deep", function() {
+		var Foo = Classy.extend({
+			x: 12
+		});
+		var Bar = Classy.extend({
+			x: 8
+		});
+		expect(Foo.x).eql(undefined);
+		expect(Bar.x).eql(undefined);
+		var foo = new Foo;
+		var bar = new Bar;
+		expect(foo.x).eql(12);
+		expect(bar.x).eql(8);
 	});
 
 });
