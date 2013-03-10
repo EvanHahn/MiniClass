1fbe22eb649006860721e38e45c9c48308af12ad
Evan Hahn <me@evanhahn.com>
2013-03-10 02:43:14 -1000

    Simplify code -- the bulk of it was in a useless function wrapper

--- classy.js
+++ classy.js
@@ -1,5 +1,8 @@
 ;(function() {
 
+	// First, define some useful things.
+	var noop = function() {};
+
 	// `Classy` is just the base class for everything, and its constructor is
 	// empty.
 	var Classy = function() {};
@@ -7,66 +10,73 @@
 	// `extend()` is the exciting method that's inherited by everything.
 	Classy.extend = function(hash) {
 
-		// Generate the resulting object.
-		return (function(parent, ctor) {
+		// Default hash is {}.
+		hash || (hash = {});
 
-			// If no constructor is defined, define an empty one.
-			if (!ctor)
-				ctor = function() {};
+		// For clarity, alias "parent".
+		var parent = this;
 
-			// Create the constructor, which calls the parent constructor and the
-			// child one.
-			var Class = function() {
-				ctor.apply(this, arguments);
-			};
+		// Build the constructor.
+		// If we don't have one, steal the parent's, or a noop if we're extending
+		// `Classy`.
+		var ctor = hash.initialize;
+		if (!ctor) {
+			ctor = parent.prototype.initialize || noop;
+		}
 
-			// Inherit all the properties from the parent.
-			for (var property in parent) {
-				if (property !== "prototype")
-					Class[property] = parent[property];
+		// Create the constructor, which calls the parent constructor and the
+		// child one.
+		var Class = function() {
+			ctor.apply(this, arguments);
+		};
+
+		// Inherit all the class methods/properties from the parent.
+		for (var property in parent) {
+			if (property !== "prototype")
+				Class[property] = parent[property];
+		}
+
+		// Inherit all the instance methods/properties.
+		for (var property in parent.prototype)
+			Class.prototype[property] = parent.prototype[property];
+
+		// Get all the properties from the new hash.
+		for (var property in hash) {
+
+			// If it's not a method, don't even worry about it.
+			if (typeof hash[property] !== "function") {
+				Class.prototype[property] = hash[property];
 			}
-			for (var property in parent.prototype)
-				Class.prototype[property] = parent.prototype[property];
 
-			// Get all the properties from the new hash.
-			for (var property in hash) {
+			// It's a function -- a little more difficult.
+			else {
 
-				// If it's not a method, don't even worry about it.
-				if (typeof hash[property] !== "function") {
-					Class.prototype[property] = hash[property];
+				// If we already have a property in there that's a function, the child
+				// needs a `super()` method in there.
+				if (typeof parent.prototype[property] === "function") {
+					Class.prototype[property] = function() {
+						var self = this;
+						this.super = function() {
+							parent.prototype[property].apply(self, arguments);
+						};
+						return hash[property].apply(this, arguments);
+					};
 				}
 
-				// It's a function -- a little more difficult.
+				// We didn't have that existing parent function, so we're good.
 				else {
-
-					// If we already have a property in there that's a function, the child
-					// needs a `super()` method in there.
-					if (typeof parent.prototype[property] === "function") {
-						Class.prototype[property] = function() {
-							var self = this;
-							this.super = function() {
-								parent.prototype[property].apply(self, arguments);
-							};
-							return hash[property].apply(this, arguments);
-						};
-					}
-
-					// We didn't have that existing parent function, so we're good.
-					else {
-						Class.prototype[property] = function() {
-							this.super = function() {};
-							return hash[property].apply(this, arguments);
-						};
-					}
-
+					Class.prototype[property] = function() {
+						this.super = noop;
+						return hash[property].apply(this, arguments);
+					};
 				}
 
 			}
 
-			// Return the constructor (with attached prototype stuff).
-			return Class;
+		}
 
-		})(this, hash.initialize);
+		// Return the constructor (with attached prototype stuff).
+		return Class;
 
 	};
 
