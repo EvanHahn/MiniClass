878e0051be85f8cd6f618abdf69fe3dd41819561
Evan Hahn <me@evanhahn.com>
2013-03-09 21:21:50 -1000

    first test is passing
    I was having some issues getting expect.js to work, but then I looked at
    the (straightforward) source and all was well.

--- test/test.js
+++ test/test.js
@@ -3,12 +3,14 @@ var expect = require("expect.js");
 var Classy = require("../classy.js");
 
 describe("simple instantiation", function() {
+
 	it("should allow classes to be created", function() {
 		var foo = new Classy;
 		var bar = new Classy;
 		foo.x = 12;
 		bar.x = 8;
-		expect(foo.x).toEqual(12);
-		expect(bar.x).toEqual(8);
+		expect(foo.x).equal(12);
+		expect(bar.x).equal(8);
 	});
+
 });
