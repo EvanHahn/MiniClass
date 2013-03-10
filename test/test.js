5b0545a501cafb1430c3dcf04ee5532f6671f661
Evan Hahn <me@evanhahn.com>
2013-03-10 01:47:25 -1000

    Improve test stuff

--- test/test.js
+++ test/test.js
@@ -11,23 +11,32 @@ describe("Classy", function() {
 		var bar = new Classy;
 		foo.x = 12;
 		bar.x = 8;
+		expect(foo).not.equal(bar);
 		expect(foo.x).eql(12);
 		expect(bar.x).eql(8);
 	});
 
 	it("allows subclasses to be created, one level deep", function() {
 		var Foo = Classy.extend({
-			x: 12
+			x: 12,
+			thing: function() {
+				return "I am foo!";
+			}
 		});
 		var Bar = Classy.extend({
-			x: 8
+			x: 8,
+			thing: function() {
+				return "I am bar.";
+			}
 		});
 		expect(Foo.x).eql(undefined);
 		expect(Bar.x).eql(undefined);
 		var foo = new Foo;
 		var bar = new Bar;
 		expect(foo.x).eql(12);
+		expect(foo.thing()).eql("I am foo!");
 		expect(bar.x).eql(8);
+		expect(bar.thing()).eql("I am bar.");
 	});
 
 });
